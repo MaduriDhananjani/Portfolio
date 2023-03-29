d89872692db05327754625784c13c95b6fae23a2
Maduri Dananjani <dananjanimaduri04@gmail.com>
2023-03-29 09:13:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+    String: ["Front Developer", "Back Developer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+
+
+})
